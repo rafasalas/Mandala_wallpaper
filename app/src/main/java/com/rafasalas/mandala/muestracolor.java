0b04db364e66b5569d250a8643b19489ae18da05
salas <rafa.salas@gmail.com>
2016-06-24 13:50:19 +0200

    New icons and view

--- app/src/main/java/com/rafasalas/mandala/muestracolor.java
+++ app/src/main/java/com/rafasalas/mandala/muestracolor.java
@@ -39,19 +39,36 @@ public class muestracolor extends View {
         //canvas.drawARGB(255, rojo, verde, azul);
         float width=canvas.getWidth();
         float height=canvas.getHeight();
-        float radius=width*0.4f;
+        float radius=width*0.2f;
+        float extreme;
+        float ladusX, ladusY, ladus, angulus;
+        float size=width*0.05f;
+        //angulus=(float)(Math.PI)/8;
         canvas.drawARGB(255, 0,0,0);
         paint.setARGB(255, rojo, verde, azul);
-        paint.setStrokeWidth(2);
+        paint.setStrokeWidth(3);
 
-        for (int i=0;i<8;i++){
+        //canvas.drawCircle(width/2, height/2,radius, paint);
+        //canvas.drawCircle(width/2, height/2,width*0.4f, paint);
+       //
+        for (int i=0;i<24;i++){
+            if (i==8){angulo=0; radius=width*0.4f;size=width*0.03f;}
+            //ladus=(float)2.91/(float)(2*radius);
+            //ladusX=(float)(ladus*Math.sin(angulus));
+            //ladusY=(float)(ladus*Math.cos(angulus));
+           //ladus=3;
+
             canvas.save();
 
             canvas.translate(width/2, height/2);
-            canvas.rotate(angulo);
-            canvas.drawCircle(radius,0,width*0.07f, paint);
 
-            canvas.drawLine(0,0, radius,0,paint);
+            canvas.rotate(angulo);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(0, radius,size, paint);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawArc(-radius, -radius, radius,radius, 0,45,false,paint);
+            //canvas.rotate(-30);
+           //canvas.drawLine( 0, radius,ladusX,radius-ladusY,paint);
             canvas.restore();
             angulo=angulo+angulobase;
 

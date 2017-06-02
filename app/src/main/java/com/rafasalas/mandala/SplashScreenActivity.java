14e7822e7ed8b9e0452c951230d8da4e6d50a6d9
salas <rafa.salas@gmail.com>
2017-06-02 14:06:56 +0200

    mod

--- app/src/main/java/com/rafasalas/mandala/SplashScreenActivity.java
+++ app/src/main/java/com/rafasalas/mandala/SplashScreenActivity.java
@@ -34,8 +34,9 @@ public class SplashScreenActivity extends Activity {
 
                 // Start the next activity
                 Intent mainIntent = new Intent().setClass(SplashScreenActivity.this, MainActivity.class);
-                startActivity(mainIntent);
+
                 overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
+                startActivity(mainIntent);
                 // Close the activity so the user won't able to go back this
                 // activity pressing Back button
                 finish();
